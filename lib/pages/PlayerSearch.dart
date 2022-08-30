89ff8346d23299dd1eb48b8d83df7b645d80cb72
Priyanka Bharade <priyankabharade7apr@gmail.com>
2022-08-30 17:32:39 +0530

    add variables in dart classes

--- lib/pages/PlayerSearch.dart
+++ lib/pages/PlayerSearch.dart
@@ -6,6 +6,7 @@ class PlayerSearch extends SearchDelegate{
  List<userDetails> userList;
 
   PlayerSearch(this.userList);
+  var test = "Priyanka2";
  
   @override
   List<Widget> buildActions(BuildContext context) {
